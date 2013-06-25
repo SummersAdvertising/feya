dfc36a84e26915bd2dc1acf2ab95c1bfccd2068a
HanaHsu <kobanae@gmail.com>
2013-06-25 18:29:34 +0800

    default address

--- app/controllers/service/addressbooks_controller.rb
+++ app/controllers/service/addressbooks_controller.rb
@@ -11,15 +11,11 @@ class Service::AddressbooksController < ApplicationController
 		@address = current_member.addressbooks.new(params[:addressbook])
 
 		respond_to do |format|
-			if (current_member.addressbooks.count < 5 && @address.save)
+			if (@address.save)
 				format.html { redirect_to  service_addressbooks_path }
 				format.json { render json: @address, status: :created, location: @address }
 			else
 				@addresses = Addressbook.where(:member_id => current_member.id).all
-
-				if (current_member.addressbooks.count >= 5)
-					flash[:alert] = "can only save 5 addresses"
-				end
 
 				format.html { render action: "index" }
 				format.json { render json: @address.errors, status: :unprocessable_entity }
--- app/models/addressbook.rb
+++ app/models/addressbook.rb
@@ -4,5 +4,13 @@ class Addressbook < ActiveRecord::Base
   validates :address, :uniqueness => true
 
   belongs_to :member
+
+  validate :record_limit, :on => :create
+
+  def record_limit
+    if self.member.addressbooks(:reload).count >= 5
+      errors.add(:base, "can only has 5 addresses in book")
+    end
+  end
     
 end
