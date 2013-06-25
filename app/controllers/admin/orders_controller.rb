54c5b5d934a6a58c85ea5d1c0b96915023e6cd2e
HanaHsu <kobanae@gmail.com>
2013-06-25 15:16:44 +0800

    discount points

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -24,15 +24,43 @@ class Admin::OrdersController < AdminController
 
     params[:orderitem].each do |orderitem|
       @orderitem = Orderitem.where(:id => orderitem[0]).select("orderitems.* ,products.name").joins('LEFT OUTER JOIN stocks on stocks.id = orderitems.stock_id LEFT OUTER JOIN products on products.id = stocks.product_id').first
+      @refundSum = 0
       if(@orderitem.amount != orderitem[1].to_i)
         @orderlog = Orderlog.new
         @orderlog.order_id = @order.id
         @orderlog.description = @orderitem.name + ((orderitem[1].to_i - @orderitem.amount) > 0 ? "增加" : "減少") + (@orderitem.amount - orderitem[1].to_i).abs.to_s + "個。"
 
         if(@orderlog.save)
+          #substract
+          if(@orderitem.amount > orderitem[1].to_i)
+            @refundSum = @refundSum + (@orderitem.amount - orderitem[1].to_i) * @orderitem.itemprice
+          end
+
           @orderitem.amount = orderitem[1].to_i
           @orderitem.save
         end
+      end
+
+      if(@refundSum > 0 && @order.discountpoint > 0 )
+
+        @substract = (@refundSum / 200).floor
+
+        if(@order.discountpoint > @substract)
+          @order.discountpoint = @order.discountpoint - @substract
+        else
+          @substract = @order.discountpoint
+          @order.discountpoint = 0
+        end
+
+        @order.save
+        
+        @member = Member.find(@order.member_id)
+        @member.discountpoint = (@member.discountpoint - @substract) > 0 ? (@member.discountpoint - @substract) : 0
+        @member.save
+
+        @orderlog = @order.orderlogs.new
+        @orderlog.description = "退貨扣除購物金：" + @substract.to_s + "點"
+        @orderlog.save
       end
     end
 
