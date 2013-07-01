b65769ec26894336075fb7942aeec90fec06106a
HanaHsu <kobanae@gmail.com>
2013-07-01 15:41:14 +0800

    admin/refund

--- app/controllers/service/orders_controller.rb
+++ app/controllers/service/orders_controller.rb
@@ -19,17 +19,20 @@ class Service::OrdersController < ApplicationController
 	def update
 		@order = current_member.orders.find(params[:id])
 
-		#order log
-		@orderlog = @order.orderlogs.new
-        @orderlog.description = "通知已匯款。"
-        @orderlog.save
+		if(params[:order][:payaccount].length>0 && params[:order][:paydate].length>0 && params[:order][:paytime].length>0)
+			@order.update_attributes(params[:order])
+			
+			#order log
+			@orderlog = @order.orderlogs.new
+			@orderlog.description = "通知已匯款。"
+			@orderlog.save
 
-		respond_to do |format|
-			if ( @order.update_attributes(params[:order]) )
-				format.html { redirect_to service_order_path(@order), notice: 'Order was successfully updated.' }
-			else
-				format.html { render "show" }
-				format.json { render json: @order.errors, status: :unprocessable_entity }
+			respond_to do |format|
+				format.html { redirect_to service_order_path(@order) }
+			end
+		else
+			respond_to do |format|
+				format.html { redirect_to service_order_path(@order), alert: '請完整輸入匯款資訊。' }
 			end
 		end
 	end
@@ -43,7 +46,7 @@ class Service::OrdersController < ApplicationController
 			if ( @orderrefund.save )
 				#寄信
 				Ordermailer.refund(current_member.email, @orderrefund).deliver
-				format.html { redirect_to service_order_path(@order), notice: 'Order was successfully updated.' }
+				format.html { redirect_to service_order_path(@order), notice: '您的問題已送出，請等待客服人員與您聯繫。' }
 			else
 				@orders = current_member.orders
 				format.html { redirect_to service_order_path(@order), alert: '請輸入問題描述' }
