ec914c2f991269da15e7041648124a93f4736cc0
Yuzhe <yuzhe.c85@gmail.com>
2013-12-05 16:07:45 +0800

    applied cart page style

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,13 +6,13 @@ class CategoriesController < ApplicationController
 	
 	def show
 			
-    @order = Order.new
-    @products = Product.where(:status => "上架").all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @products }
-    end
+	    @order = Order.new
+	    @products = Product.where(:status => "上架").all
+	
+	    respond_to do |format|
+	      format.html # index.html.erb
+	      format.json { render json: @products }
+	    end
 		
 	end
 end
