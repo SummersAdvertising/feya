c3eab268cac0f3e81d59091d85cec0d30a76ac84
Yuzhe <yuzhe.c85@gmail.com>
2014-01-08 18:00:41 +0800

    adjust news order

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,7 +12,7 @@ class EntriesController < ApplicationController
 private
 	def get_grouped_entries
 		@entries = Entry.order( "created_at desc" )
-		@entries_grouped = Entry.all.group_by { | e | e.created_at.beginning_of_month }		
+		@entries_grouped = Entry.order( "created_at desc" ).group_by { | e | e.created_at.beginning_of_month }		
 	end
 
 end
