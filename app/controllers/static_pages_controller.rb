27e26af1072c0f1cc0f4b61df8b019ff36711345
Yuzhe <yuzhe.c85@gmail.com>
2014-01-08 11:07:35 +0800

    fix news order and order pages auto height

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
 	before_filter :clear_flash
 
 	def index
-		@entries = Entry.limit(3)
+		@entries = Entry.order( :created_at => :desc ).limit(3)
 		
 		@products = Product.where( "status = '上架' AND delete_flag IS NULL" ).limit(4)
 		
