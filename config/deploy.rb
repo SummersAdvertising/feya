3d0e8f3b14f18c94dcceaad3c58368d9efa4a832
Yuzhe <yuzhe.c85@gmail.com>
2014-01-09 17:41:01 +0800

    test delayed job

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,3 +8,9 @@ set :default_stage, "staging"
 set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
 set :keep_releases, 5
 after "deploy:update", "deploy:cleanup" 
+
+require "delayed/recipes"  
+
+after "deploy:stop",    "delayed_job:stop"
+after "deploy:start",   "delayed_job:start"
+after "deploy:restart", "delayed_job:restart"
